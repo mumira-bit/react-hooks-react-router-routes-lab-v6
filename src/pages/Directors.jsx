7698d22ae3d29ac83f64890761961727f8fa5a6d
mumira-bit <mumiraesther41@gmail.com>
2025-07-28 11:44:34 +0300

    react-router

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,16 +1,48 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const directors = [
+  {
+    name: "Mike Mitchell",
+    movies: ["Trolls", "Sky High"]
+  },
+  {
+    name: "Jennifer Lee",
+    movies: ["Frozen", "Frozen II", "Doctor Strange"]
+  },
+  {
+    name: "Edward Zwick",
+    movies: ["Jack Reacher: Never Go Back", "Blood Diamond", "The Siege"]
+  },
+  {
+    name: "Scott Derrickson",
+    movies: ["Doctor Strange"]
+  }
+];
 
 function Directors() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        <ul>
+          {directors.map((director) => (
+            <li key={director.name}>
+              <h2>{director.name}</h2>
+              <ul aria-label={`${director.name}'s movies`}>
+                {director.movies.map((movie) => (
+                  <li key={movie}>{movie}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
-};
+}
 
 export default Directors;
